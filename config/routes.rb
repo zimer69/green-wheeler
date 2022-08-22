ac399860064ba9f0f3937420ac02f447b4032bcb
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-22 17:21:52 -0300

    installed Devise Gem and tested User Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
 end
