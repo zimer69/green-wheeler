7186ee923130e5b7cb8ead8aff0dac15af5ff9d1
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-23 20:01:47 -0300

    Users seed create. Working on Offers seed. Commit to update master.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "[1/3] Clearing database..."
+Offer.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+
+puts "[2/3] Generating seed..."
+4.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Food.fruits,
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    rating: rand(0..5),
+    user_type: %w[customer owner].sample
+  )
+  p user
+  5.times do
+    p Offer.create!(
+      category: ['Bicycle', 'Skateboard', 'Scooter', 'Rollerblades'].sample,
+      price_in_cents: Faker::Number.number(digits: 10),
+      user_id: user.id,
+      rating: rand(0..5),
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.sentence(word_count: 10),
+      electric: [true, false].sample,
+      safety_equipment: [true, false].sample,
+      optional: ['None', 'Padlock', 'Backseat'].sample
+    )
+  end
+end
+
+puts "[3/3] Seed successfully created!"
