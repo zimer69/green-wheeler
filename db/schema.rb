a43ca0d783655bf288dfeba4531488ddadebb7c1
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-23 20:24:51 -0300

    Setted default values for Electric and Safety Equipment columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_163038) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_232248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_163038) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
-    t.boolean "electric"
-    t.boolean "safety_equipment"
+    t.boolean "electric", default: false
+    t.boolean "safety_equipment", default: false
     t.string "optional"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
