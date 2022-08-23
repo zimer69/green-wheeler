fd0e719aff36ded356674ff889d2c990bef848e6
Joana Areas <jo_areas@hotmail.com>
2022-08-23 11:47:01 -0300

    Added validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :description, uniqueness: { scope: :title,
+    message: "There is already an offer with this tittle and description." }
 end
