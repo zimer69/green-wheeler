99f0c3580a97f43ccd2238d7f801471da4df7db3
ana brasil <apcbrasil@gmail.com>
2022-08-23 13:02:51 -0300

    updating user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers
 
+  validates :first_name, :last_name, presence: true
+  validates :user_type
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
