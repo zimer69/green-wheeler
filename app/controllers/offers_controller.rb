822732a48b3497ed46eea1fb93a3ca405244501d
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-24 16:12:59 -0300

    show offer page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,6 @@
 class OffersController < ApplicationController
   before_action :set_offer, only: %i[show edit update destroy]
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @offers = policy_scope(Offer)
