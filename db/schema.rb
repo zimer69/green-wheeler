1dbec72f5315d06b0b4d0592bf53a5f4815602f2
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-25 11:16:45 -0300

    removed user type from users, removed duplicated end file inside gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_025603) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_141547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,7 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_025603) do
     t.string "first_name"
     t.string "last_name"
     t.float "rating"
-    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
